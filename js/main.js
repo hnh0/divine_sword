cebb6f09c88467502a58079d8cd159e1455c939a
Howe <hhhaohao@163.com>
2015-12-29 02:11:53 +0800

    btn & figure obj

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+var figure = {
+	0: '<p>oL的召唤师拥有很多MMORPG网游中才会出现的天赋系统，在召唤者信息面板中点击天赋就可以看到3系天赋树。每系的终极天赋均为21点，召唤师等级每提升1级，就可以多获得一点天赋点，用以自由学习天赋，最多为30点，召唤师的天赋并非是固定的，根据你选择控制的英雄不同，你需要修改天赋以适应召唤师的需要，点击界面右下方的“重置天赋点”可以进行洗点。</p><p>oL的召唤师拥有很多MMORPG网游中才会出现的天赋系统，在召唤者信息面板中点击天赋就可以看到3系天赋树。每系的终极天赋均为21点，召唤师等级每提升1级，就可以多获得一点天赋点，用以自由</p>',
+	1: '<p>1</p>',
+	2: '<p>2</p>',
+	3: '<p>3</p>',
+	4: '<p>4</p>',
+	5: '<p>5</p>',
+	6: '<p>6</p>',
+	7: '<p>7</p>',
+	8: '<p>8</p>',
+	9: '<p>9</p>',
+	10: '<p>10</p>',
+	11: '<p>11</p>',
+	12: '<p>12</p>',
+};
 var sword = window.sword || {};
 (function () {
 	sword = {
@@ -39,6 +54,10 @@ var sword = window.sword || {};
 			$('.light_model').addClass('hidden');
 		},
 		showFigure: function (index) {
+			var index = Number(index)
+			var text = figure[index];
+			$('.figure_img').attr('src', 'img/figure/figure_' + index + '.png')
+			$('.figure_main').html(text);
 			$('.figure_detail').removeClass('hidden');
 		},
 		showList: function () {
@@ -61,7 +80,8 @@ var sword = window.sword || {};
 	});
 	$('.figure_list_item').on('click',function(){
 		$(this).closest('.figure_list').addClass('hidden');
-		sword.showFigure();
+		var index = $(this).attr('index');
+		sword.showFigure(index);
 	});
 	$('.figure_detail').on('click', function () {
 		$(this).addClass('hidden');
